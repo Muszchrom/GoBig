c9553e642346d9e77cb09c34288fd99fd6e4cb60
Muszchrom <altair2468@o2.pl>
2023-08-15 02:48:44 +0200

    fixed calss name appeareance in dev tools

--- client/src/components/Semester.js
+++ client/src/components/Semester.js
@@ -159,8 +159,7 @@ function DayComponent({day, monthId}) {
                 className={`${dayState.type !== 0 
                             && dayState.type !== 1 
                             || dayState.message.length 
-                            ? "cellOfType" : ""}
-                            ${show && "cellActive"}`}>
+                            ? "cellOfType" : ""}${show ? " cellActive" : ""}`}>
                 {date.getDate()}
                 {show && <ContextWindow handleClose={setShow} 
                                         data={{title: dateTitle, message: dayState.message, type: dayState.type}} 
